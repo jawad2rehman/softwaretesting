929ed978a98dbb110c18f5fe793cf6b3640d8a31
Jawad <Jawad@DESKTOP-7OFVVT9>
2020-05-27 23:39:42 +0200

    Added an integration test

--- Assignment3/src/test/java/TestsIT.java
+++ Assignment3/src/test/java/TestsIT.java
@@ -19,5 +19,19 @@ public class TestsIT {
 		
 		Assert.assertEquals(classB.asqaureminusbsqaure(a, b), expected, 0.00001);
 	}
+	@Test
+	public void IntegrationTest2() {
+		
+		// we will provide output of one class as input to second class and test the working
+		ArithmaticFunctions classA = new ArithmaticFunctions();
+		AlgebraicFunctions classB = new AlgebraicFunctions();
+		
+		double a = classA.Add(1, 1);
+		double b  = classA.Add(2, 1);
+		
+		double expected = 25;
+		
+		Assert.assertEquals(classB.aplusbwholesquare(a, b), expected, 0.00001);
+	}
 
 }
