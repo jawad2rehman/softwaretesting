f62c1880e52e9f6bda9bbebefb1e816fb020ce79
Jawad <Jawad@DESKTOP-7OFVVT9>
2020-05-27 22:49:52 +0200

    added a new unit test

---
+++ Assignment3/src/test/java/AlgebraicUnitTests.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AlgebraicUnitTests {
+
+	@Test
+	public void testAlgebraic1() {
+		//Happy Path unit test
+		double a = 2;
+		double b = 1;
+		double expected = 3;
+		AlgebraicFunctions obj = new AlgebraicFunctions();
+		Assert.assertEquals(obj.asqaureminusbsqaure(a, b), expected, 0.00001);
+	}
+
+}
